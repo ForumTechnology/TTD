ddc0b09f8c62ab8177572fc048106e8f48e0ad0d
DienTon <thatdien295@gmail.com>
2024-02-29 17:37:44 +0700

    viewBlog

--- security/src/main/java/com/example/security/config/MyWebMvcConfigurer.java
+++ security/src/main/java/com/example/security/config/MyWebMvcConfigurer.java
@@ -1,8 +1,15 @@
 package com.example.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
